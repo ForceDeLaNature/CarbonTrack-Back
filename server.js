2d292f05098b9543c7d563b7421bb3130328ac4a
Marcelin <marcelintingougoui3@gmail.com>
2025-03-28 16:06:13 +0100

    Modification de la configuration de l'hôte dans server.js pour utiliser la variable d'environnement HOST, avec une valeur par défaut de 127.0.0.1.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routeGetCarbonne = require('./src/routes/transport.router');
 const path = require("path");
 require("dotenv").config();
 
-const hostname = "127.0.0.1";
+const hostname = process.env.HOST || "127.0.0.1";
 const port = process.env.PORT || 3000;
 
 const app = express();
